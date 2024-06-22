2e9a120f39071112aae2e0121af7f2eda995f674
Atkh0405 <Atifkhan14670@gmail.com>
2024-06-22 20:54:58 +0500

    Created Number Guessing Game

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "please guess a number between 1-10",
+        message: "please guess a number between 1-6",
     }
 ]);
 if (answers.userGuessedNumber === randommNumber) {
