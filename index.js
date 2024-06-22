637705089c22c815100bcf2323212eef88fd2a9c
Atkh0405 <Atifkhan14670@gmail.com>
2024-06-22 20:44:40 +0500

    Created Number Guessing Game

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// // 1) Coputer will generate a random number
+// // 2) User input for guesing number
+// // 3) Computer user input with computer generated number and show result - done
+const randommNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1-10",
+    }
+]);
+if (answers.userGuessedNumber === randommNumber) {
+    console.log("Congratulations!You guessed right number");
+}
+else {
+    console.log("You gussed wrong number");
+}
